385040656a45ed459babf804ce640552e3b46a09
yk <y6ulli@gmail.com>
2020-06-05 21:36:18 +0700

    change a

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href="/profile">Profile</a>
+                <NavLink to="/profile">Profile</NavLink>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs">Messages</NavLink>
             </div>
             <div className={s.item}>
-                <a href="/news">News</a>
+                <NavLink to="/news">News</NavLink>
             </div>
             <div className={s.item}>
-                <a href="music">Music</a>
+                <NavLink to="music">Music</NavLink>
             </div>
             <div className={s.item}>
-                <a href="settings">Settings</a>
+                <NavLink to="settings">Settings</NavLink>
             </div>
         </nav>
     )
